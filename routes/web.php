6453ca050c8bb2d764e849c889e3aa26cc44d75c
Michael okoh <ordrizzy@gmail.com>
2019-06-10 16:06:49 +0100

    All the routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('upload-file', 'HomeController@index')->name('home');
+
+Route::view('file-upload', 'file-upload');
+
+Route::post('upload-file', 'FileRefController@upload');
+
+Route::get('all', 'FileRefController@listAll');
+
+Route::get('download/{ref}', 'FileRefController@download');
