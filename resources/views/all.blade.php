802d640c2f05f74771e925cb4b0852d2a48e92fd
Michael okoh <ordrizzy@gmail.com>
2019-06-10 16:09:28 +0100

    all files view

---
+++ resources/views/all.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Encrypted Files</div>
+
+                <div class="card-body">
+                  <div class="container">
+                    <table class="table">
+                      <thead>
+                        <tr>
+                          <th>File Name</th>
+                          <th>Download Link</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        @foreach($infos as $info)
+                        <tr>
+                          <td>{{$info->filename}}</td>
+                          <td><a href="/download/{{$info->ref}}"><button class="btn btn-primary">Decrypt and Download</button></a></td>
+                        </tr>
+                        @endforeach
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
